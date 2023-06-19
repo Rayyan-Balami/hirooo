1e3b0fe9ad0ec88fa53861153668d134a45ee5d3
Rayyan Balami <visionrayyan60517@gmail.com>
2023-06-19 16:47:47 +0545

    college

---
+++ day6/day6_JS/fundamental.js
@@ -0,0 +1,16 @@
+let a = "hir000"
+console.log("${a}");
+console.log(`rayyan balami ${a}`)
+console.log(`rayyan balami ${a} ,` ,Math.pow(2,2))
+
+let array = ['a','e','i','o','u'];
+
+let num;
+for(let i = 0; i<5; i++)
+{
+    console.log(array[i]);
+}
+array.forEach(function(i)
+{
+    console.log(i);
+})
